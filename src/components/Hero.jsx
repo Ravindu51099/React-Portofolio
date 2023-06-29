9be4267b747d14f2d088876b096a8109684f0662
Ravindu Dissanayaka <malisha.ponweera@towcha.com.au>
2023-06-29 17:02:02 +0530

    hero page updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Ravindu</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop interfaces <br className="sm:block hidden" />& web
+            I develop <br className="sm:block hidden" />& web
             applications
           </p>
         </div>
