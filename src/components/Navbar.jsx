994692fa9335af318843795476d4fb01236c3360
Ravindu Dissanayaka <malisha.ponweera@towcha.com.au>
2023-06-29 16:23:24 +0530

    modifications

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Adrian &nbsp;
-            <span className='sm:block hidden'> | JavaScript Mastery</span>
+            Ravindu &nbsp;
+            <span className='sm:block hidden'> Dissanayaka</span>
           </p>
         </Link>
 
